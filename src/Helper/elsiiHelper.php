ac2eeec7823d2d784c1e05324ed8b6e945a7652a
Andrey <a.cherewaty@gmail.com>
2020-05-29 15:51:18 +0300

    add function for work

--- src/Helper/elsiiHelper.php
+++ src/Helper/elsiiHelper.php
@@ -6,6 +6,7 @@ namespace Helper;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
+use DateTime;
 
 class elsiiHelper
 {
@@ -47,4 +48,102 @@ class elsiiHelper
 //        $body = $response->getBody();
 //        var_dump($headers, $body);
 //    }
+
+
+    /**
+     * Функция для получения разницы во времени
+     *
+     * @param $time_start
+     * @param $time_end
+     * @return \DateInterval|false
+     */
+    public static function getDateDiff($time_start, $time_end)
+    {
+        $_date_start = date('Y-m-d H:i:s', $time_start);
+        $_date_end = date('Y-m-d H:i:s', $time_end);
+
+        $date_start_create = date_create($_date_start);
+        $date_end_create = date_create($_date_end);
+
+        return date_diff($date_start_create, $date_end_create);
+    }
+
+    /**
+     * Получаем процент от числа
+     *
+     * @param int $number
+     * @param float $percent
+     * @return float|int
+     */
+    public static function getPercentToNumber(int $number,float $percent) {
+        return ($number * $percent) / 100;
+    }
+
+    /**
+     * Функция для убирания бита с числа
+     *
+     * @param int $result_bit
+     * @param int $bit
+     * @return int
+     */
+    public static function softRemBit(int $result_bit,int $bit) {
+        // Если бит установлен то мы его снимаем
+        if($result_bit & $bit)
+            $result_bit ^= $bit;
+
+        return $result_bit;
+    }
+
+    /**
+     * Функция для установки бита
+     *
+     * @param int $result_bit
+     * @param int $bit
+     * @return int
+     */
+    public static function softSetBit(int $result_bit,int $bit) {
+        // Если бит установлен то мы возвращаем биты
+        if($result_bit & $bit)
+            return $result_bit;
+
+        $result_bit |= $bit;
+
+        return $result_bit;
+    }
+
+    /**
+     * Функция для получения DateTime
+     *
+     * @param $timestamp integer
+     * @param $format string
+     * @return DateTime
+     * @throws \Exception
+     */
+    public static function getDateTime($timestamp, $format = 'Y-m-d H:i:s') {
+        return new DateTime(date($format, $timestamp));
+    }
+
+    /**
+     * Функция которая проверяет вхождения времени
+     * мы проверяем время начала работы и окончания если в этот промежуток попадает аренда то мы выводим true иначе false
+     * @param $unix_from - время начала работы
+     * @param $unix_to - время окончания работы
+     * @param $time_start_rent - время начала аренды
+     * @param $time_end_rent - время окончания аренды
+     * @param $is_debug - дебаг времени
+     * @return bool
+     */
+    public static function checkTimeRent($unix_from, $unix_to, $time_start_rent, $time_end_rent, $is_debug = false) {
+        if($is_debug)
+            print ("unix_from -> ". date('Y-m-d H:i:s', $unix_from) ." >= start_rent -> ". date('Y-m-d H:i:s', $time_start_rent) ." && unix_to -> ". date('Y-m-d H:i:s', $unix_to) ." <= end_rent -> ". date('Y-m-d H:i:s', $time_end_rent) ."" . PHP_EOL);
+
+        if(
+            ($unix_from >= $time_start_rent && $unix_to <= $time_end_rent)
+            || ($time_start_rent >= $unix_from && $unix_to <= $time_end_rent && $time_start_rent < $unix_to)
+            || ($unix_from >= $time_start_rent && $unix_to >= $time_end_rent && $time_end_rent > $unix_from && $time_end_rent < $unix_to)
+        )
+            return true;
+
+        return false;
+    }
 }
